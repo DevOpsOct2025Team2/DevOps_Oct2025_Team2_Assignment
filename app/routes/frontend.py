f1f5c6dc10c50e836b37e49da881e0e0b6847aa3
qixuaann <s10269128@connect.np.edu.sg>
2026-02-10 10:32:57 +0800

    Fixed overlapping

--- app/routes/frontend.py
+++ app/routes/frontend.py
@@ -3,10 +3,3 @@ from flask import Blueprint, render_template
 
 frontend_bp = Blueprint('frontend', __name__)
 
-@frontend_bp.route('/')
-def index():
-    return render_template('login.html', title='Login', page='login')
-
-@frontend_bp.route('/dashboard')
-def dashboard():
-    return render_template('admin_dashboard.html', title='Dashboard', page='dashboard')
